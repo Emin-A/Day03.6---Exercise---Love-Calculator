879ef8896498159edda898f77575d01ff330b7fd
Emin Avdovic <72197800+Emin-A@users.noreply.github.com>
2022-04-24 20:20:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# Make a Love Calculator! 
+# .lower() and .count()
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+
+# Angela.lower()
+# Angela.count("a")
+# lower_case_name = "Angela".lower()
+# lower_case_name.count("a")
+
