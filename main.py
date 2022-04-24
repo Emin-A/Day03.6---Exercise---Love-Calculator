67ffe0ee476f5f750083aae915513bacea335bc2
Emin Avdovic <72197800+Emin-A@users.noreply.github.com>
2022-04-24 20:50:39 +0000

    Trying to structure the code.

--- main.py
+++ main.py
@@ -4,8 +4,22 @@ print("Welcome to the Love Calculator!")
 name1 = input("What is your name? \n")
 name2 = input("What is their name? \n")
 
-# Angela.lower()
-# Angela.count("a")
-# lower_case_name = "Angela".lower()
-# lower_case_name.count("a")
+lower_case_one = name1.lower()
+lower_case_two = name2.lower()
+
+print("T", lower_case_one.count("t") + "L", lower_case_one.count("l"))
+print("r", lower_case_one.count("r"))
+print("u", lower_case_one.count("u"))
+print("e", lower_case_one.count("e"))
+print("L", lower_case_one.count("l"))
+print("o", lower_case_one.count("o"))
+print("v", lower_case_one.count("v"))
+print("e", lower_case_one.count("e"))
+print(lower_case_two.count("t"))
+
+
+# Emin.lower()
+# Emin.count("e")
+# lower_case_name = "Emin".lower()
+# lower_case_name.count("e")
 
