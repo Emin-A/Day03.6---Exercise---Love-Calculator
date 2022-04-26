4ab582448e73229f566f97d81161100d490f7c87
Emin Avdovic <72197800+Emin-A@users.noreply.github.com>
2022-04-26 16:13:49 +0000

    Challenge Done !

--- main.py
+++ main.py
@@ -13,9 +13,17 @@ print("u", lower_case_one.count("u") + lower_case_two.count("u") , "v", lower_ca
 print("e", lower_case_one.count("e") + lower_case_two.count("e") , "e", lower_case_one.count("e") + lower_case_two.count("e"))
 
 love_score_one = lower_case_one.count("t") + lower_case_two.count("t") + lower_case_one.count("r") + lower_case_two.count("r") + lower_case_one.count("u") + lower_case_two.count("u") + lower_case_one.count("e") + lower_case_two.count("e")
-lover_score_two = lower_case_one.count("l") + lower_case_two.count("l") + lower_case_one.count("o") + lower_case_two.count("o") + lower_case_one.count("v") + lower_case_two.count("v") + lower_case_one.count("e") + lower_case_two.count("e")
+love_score_two = lower_case_one.count("l") + lower_case_two.count("l") + lower_case_one.count("o") + lower_case_two.count("o") + lower_case_one.count("v") + lower_case_two.count("v") + lower_case_one.count("e") + lower_case_two.count("e")
 
-print(f"Your score is {love_score_one}{lover_score_two}% you are meant to be together!")
+total_score = str(love_score_one) + str(love_score_two)
+
+# print(f"Your score is {total_score}% there is a spark here!")
+if int(total_score) < 10 or int(total_score) > 90:
+  print(f"Your score is {int(total_score)}%, you go together like coke and mentos!")
+elif int(total_score) in range(40,50):
+  print(f"Your score is {int(total_score)}%, you are alright together!")
+else:
+  print(f"Your score is {int(total_score)}%!")
 
 # Emin.lower()
 # Emin.count("e")
